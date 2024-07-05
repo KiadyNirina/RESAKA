2500e399d90b0064f9c519c1aaa40f94476a5b33
Kiady Nirina <kiady142ram@gmail.com>
2024-07-04 13:53:52 -1200

    Adding the login.blade.php file

---
+++ resources/views/register/login.blade.php
@@ -0,0 +1,40 @@
+@extends('base')
+
+@section('content')
+
+    <div class="w3-row w3-margin"> 
+        <div class="w3-col s6 w3-center">
+            <img id='profile' src="img/icons/giphy.gif" alt="" style="height: 80%; width: 80%;">  
+        </div>
+
+        <div class="w3-col s6 w3-padding-16">    
+            <h1><b>Connexion</b></h1>
+            <p>Veuillez entrez votre nom et mot de passe pour vous connectez!</p>
+                
+            <div class="w3-padding-16">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="">Nom : </label>
+                        <input class="w3-input w3-light-grey" type="text" name="name" id="" placeholder="Ex : John Doe">
+                    </div>
+                    
+                    <div class="form-group w3-margin-top">
+                        <label for="">Mot de passe :</label>
+                        <input class="w3-input w3-light-grey" type="password" name="password" id="" placeholder="Ex : **************">
+                    </div>
+
+                    <p><a class="w3-text-blue" href="">Mot de passe oublié?</a></p>
+                    <button class="w3-button w3-border" type="submit" name="submit">Se connecter</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="w3-center w3-black">
+        <img id="mail" src="img/icons/email.png" alt="logo" height="40">
+        <img id="fb" src="img/icons/fb.png" alt="logo" height="40">
+        <img id="insta" src="img/icons/insta.png" alt="logo" height="40">
+        <img id="tel" src="img/icons/tel.png" alt="logo" height="35">
+    </div>
+
+@endsection
