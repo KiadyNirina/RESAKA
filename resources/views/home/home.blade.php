b1b8a1cf39700ef805b342105ab991c9dcd20593
Kiady Nirina <kiady142ram@gmail.com>
2024-07-15 18:18:05 -1200

    modifications design of the home page

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -30,26 +30,47 @@
         </ul>
     </div>
 
-    <div class="content w3-half w3-border">
+    <div class="content w3-half w3-border w3-padding">
         @if( isset($posts) )
 
+            <ul>
             @foreach( $posts as $post )
-                <ul>
-                    <li>@if( Auth::id() === $post -> user_id) You @else {{ $post -> user -> name }} @endif : <br>
-                        created at : {{ $post -> created_at }}
-                        <a href="{{ route('home.post', ['id' => $post -> id]) }}">{{ $post -> description }}</a> 
+                    <li>
+                        <div class="info">
+                            <img class="user_pic" src="/users/utilisateur.png" alt="">
+                            <div class="">
+                                <span>
+                                    @if( Auth::id() === $post -> user_id) 
+                                        You 
+                                    @else 
+                                        {{ $post -> user -> name }} 
+                                    @endif 
+                                </span><br>
+                                <span id="created_at">created at : {{ $post -> created_at }}</span>
+                            </div>
+                        </div>
+                        
+                        <br>
+                        
+                        <div class="post">
+                            <a href="{{ route('home.post', ['id' => $post -> id]) }}">{{ $post -> description }}</a> 
 
-                        @if( $post -> user_id === Auth::id() )
-                        <a href="{{ route('post.update', ['id' => $post -> id]) }}"><button>Modifier</button></a>
-                        <form action="{{ route('post.delete_store', ['id' => $post -> id]) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button>Supprimer</button>
-                        </form>
-                        @endif
+                            <div class="action">
+                                @if( $post -> user_id === Auth::id() )
+                                <a href="{{ route('post.update', ['id' => $post -> id]) }}"><button class="actionButton"><img class="" src="/icons/modifier.png" alt=""></button></a>
+                                <form action="{{ route('post.delete_store', ['id' => $post -> id]) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="actionButton"><img src="/icons/supprimer.png" alt=""></button>
+                                </form>
+                                @endif
+                            </div>
+                        </div>
+                        <hr>
+        
                     </li>
-                </ul>
             @endforeach
+            </ul>
 
         @else
 
